0d412a0bfacebf68117f18aa34589d8d7dd16804
Nilesoj20 <jvrn2014@gmail.com>
2023-05-29 19:20:03 -0700

    fix: correct text error when printing

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ re = abs(number) % 10
 re *= -1 if number < 0 else 1
 
 if re > 5:
-    print(f"The string Last digit of {number} is {re} and is greater than 5")
+    print(f"Last digit of {number} is {re} and is greater than 5")
 elif re == 0:
     print(f"Last digit of {number} is {re} and is 0")
 else:
