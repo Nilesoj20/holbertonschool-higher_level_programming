4bebb8ed0a9a856c245da96776a5dd2fa418168f
Nilesoj20 <jvrn2014@gmail.com>
2023-05-31 17:29:19 -0700

    fix: correct ouput error

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,6 +5,9 @@ if __name__ == "__main__":
     ar = sys.argv
     if len(ar) == 1:
         print("0 arguments.")
-    print("{} argument:".format(len(ar)-1))
+    elif len(ar) == 2:
+        print("1 argument:")
+    elif len(ar) > 2:
+        print("{} arguments:".format(len(ar)-1))
     for i in range(1, len(ar)):
         print("{}: {}".format(i, ar[i]))
