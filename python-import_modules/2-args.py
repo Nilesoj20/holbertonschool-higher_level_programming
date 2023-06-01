c5bff629f867de37690058575c5b464da6f87528
Nilesoj20 <jvrn2014@gmail.com>
2023-05-31 17:15:44 -0700

    feat:  program that prints the number of and the list of its arguments

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    ar = sys.argv
+    if len(ar) == 1:
+        print("0 arguments.")
+    print("{} arguments.".format(len(ar)-1))
+    for i in range(1, len(ar)):
+        print("{}: {}".format(i, ar[i]))
