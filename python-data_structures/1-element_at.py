4359cf14d4d5b021f518799f4d2a1eed41c9f11b
Nilesoj20 <jvrn2014@gmail.com>
2023-06-05 18:10:26 -0700

    fix: value None

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0 or idx > len(my_list):
-        return
+        return None
     for i in my_list:
         if idx == i:
             return my_list[i]
