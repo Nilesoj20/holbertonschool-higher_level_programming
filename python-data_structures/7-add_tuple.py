d1ca87a371f5736d2f5a971e70c7366f693580df
Nilesoj20 <jvrn2014@gmail.com>
2023-06-07 14:14:37 -0700

    fix: error in other cases

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,35 +2,24 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     size_a = len(tuple_a)
     size_b = len(tuple_b)
-    a0, a1, b0, b1 = 0, 0, 0, 0
 
-    if size_a < 2:
-        if size_a == 0:
-            a0 = 0
-            a1 = 0
-            b0 = tuple_b[0]
-            b1 = tuple_b[1]
-        else:
-            a0 = tuple_a[0]
-            a1 = 0
-            b0 = tuple_b[0]
-            b1 = tuple_b[1]
-
-    if size_b < 2:
-        if size_b == 0:
-            b0 = 0
-            b1 = 0
-            a0 = tuple_a[0]
-            a1 = tuple_a[1]
-        else:
-            b0 = tuple_b[0]
-            b1 = 0
-            a0 = tuple_a[0]
-            a1 = tuple_a[1]
-
-    if size_a == size_b:
+    if size_a == 0:
+        a0 = 0
+        a1 = 0
+    elif size_a == 1:
+        a0 = tuple_a[0]
+        a1 = 0
+    else:
         a0 = tuple_a[0]
         a1 = tuple_a[1]
+
+    if size_b == 0:
+        b0 = 0
+        b1 = 0
+    elif size_b == 1:
+        b0 = tuple_b[0]
+        b1 = 0
+    else:
         b0 = tuple_b[0]
         b1 = tuple_b[1]
 
