235075e06116965dd4bd2210d094d79ea118d066
Nilesoj20 <jvrn2014@gmail.com>
2023-06-07 10:06:25 -0700

    feat: function that adds 2 tuples

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    size_a = len(tuple_a)
+    size_b = len(tuple_b)
+    a0, a1, b0, b1 = 0, 0, 0, 0
+
+    if size_a < 2:
+        if size_a == 0:
+            a0 = 0
+            a1 = 0
+            b0 = tuple_b[0]
+            b1 = tuple_b[1]
+        else:
+            a0 = tuple_a[0]
+            a1 = 0
+            b0 = tuple_b[0]
+            b1 = tuple_b[1]
+
+    if size_b < 2:
+        if size_b == 0:
+            b0 = 0
+            b1 = 0
+            a0 = tuple_a[0]
+            a1 = tuple_a[1]
+        else:
+            b0 = tuple_b[0]
+            b1 = 0
+            a0 = tuple_a[0]
+            a1 = tuple_a[1]
+
+    if size_a == size_b:
+        a0 = tuple_a[0]
+        a1 = tuple_a[1]
+        b0 = tuple_b[0]
+        b1 = tuple_b[1]
+
+    tupla_suma = (a0 + b0, a1 + b1)
+    return tupla_suma
