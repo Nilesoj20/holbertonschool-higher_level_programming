8c20bc1bdf88362350fc0893a05426205475d704
Nilesoj20 <jvrn2014@gmail.com>
2023-06-08 20:49:04 -0700

    fix: defining pycodestyle request function

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+def cuadrado(elemento):
+    return elemento ** 2
+
+
 def square_matrix_simple(matrix=[]):
     temp = []
-    cuadrado = lambda elemento: elemento ** 2
     temp = list(map(lambda fila: list(map(cuadrado, fila)), matrix))
     return temp
