952da7caf46f46443b14eaca42c98ce7afdadd33
Nilesoj20 <jvrn2014@gmail.com>
2023-06-13 21:40:16 -0700

    feat: class Square that defines a square

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" int: class Square that defines a square
+"""
+
+
+class Square:
+    """ class Square """
+    def __init__(self, size):
+        """ initialize init
+            Args:
+                size (int): Private instance attribute
+        """
+        self.__size = size
