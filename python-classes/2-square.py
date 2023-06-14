74855dcc18b10c1777e94404194e02b00b8dd749
Nilesoj20 <jvrn2014@gmail.com>
2023-06-14 09:28:34 -0700

    feat: guessing the ouput in other cases requested by the task

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-""" int: class Square that defines a square
-"""
+""" int: class Square that defines a square"""
 
 
 class Square:
     """ class Square """
     def __init__(self, size=0):
         """ initialize init
+
         Arg:
             size (int): Private instance attribute
         """
-        if type(size) is not int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.size = size
+            self.__size = size
