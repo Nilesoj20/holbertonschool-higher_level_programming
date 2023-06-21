2e77b0b1a6dea2abf22c17d2a23f77bde85b11ee
Nilesoj20 <jvrn2014@gmail.com>
2023-06-21 15:18:14 -0700

    feat: that raises an Exception with the message area() is not implemented

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" an empty class BaseGeometry """
+
+
+class BaseGeometry:
+    """ Function that raises an error message """
+
+    def area(self):
+        """Public instance method that raises an error message """
+
+        raise Exception("area() is not implemented")
