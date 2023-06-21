e2bf1d7b46520e13ac6666e5ebd3928c6dcfae93
Nilesoj20 <jvrn2014@gmail.com>
2023-06-21 16:15:57 -0700

    fix: validando documentacion

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,9 +4,15 @@
 
 
 class MyList(list):
-    """inherits from list"""
+    """Class  that inherits from list"""
+
     def print_sorted(self):
-        """prints the list, but sorted
-        (ascending sort)
+        """prints the list, but sorted (ascending sort)
+
+        Args:
+            list (list): legacy list
+
+        Return:
+            list sorted in ascending order
         """
-        print(sorted(self))""" documentacion """
+        print(sorted(self))
