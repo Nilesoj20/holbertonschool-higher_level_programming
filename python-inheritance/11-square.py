d73ae49f06ae17f9f0137db8af3736cd87d60068
Nilesoj20 <jvrn2014@gmail.com>
2023-06-21 20:51:23 -0700

    feat: printing method personal message Square

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Import parent classes from the file 9-rectangle.py """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Creates a Square class receiving size """
+
+    def __init__(self, size):
+        """ Init constructor method
+
+        Args:
+            size (int): attribute that receives the method
+
+        Return:
+            the area and the message
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """ method for printing description """
+
+        return "[Square] {}/{}".format(self.__size, self.__size)
