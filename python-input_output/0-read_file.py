091b3ba138ecd7e99f2fd78a0dc3008d5f7a39a9
Nilesoj20 <jvrn2014@gmail.com>
2023-06-22 18:28:45 -0700

    fix: ouput

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,5 +9,5 @@ def read_file(filename=""):
 
     Return:
         prints it to stdout"""
-    with open('my_file_0.txt', 'r') as file:
+    with open(filename, 'r') as file:
         print(file.read(), end="")
