00a7d96f00b58c367b32f16fb15cc6978b4913ad
Nilesoj20 <jvrn2014@gmail.com>
2023-06-24 09:25:32 -0700

    feat: requirement

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
-""" function that returns the dictionary with simple data structure"""
+""" function that returns the dictionary with simple data structure
+"""
 import json
 
 
 def class_to_json(obj):
-    """converts the given object into a dictionary """
+    """converts the given object into a dictionary
+
+    Args:
+        obj (class): object
+
+    Returns:
+        a dictionary class object
+    """
     return obj.__dict__
