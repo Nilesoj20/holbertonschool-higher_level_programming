7635cf6948f88912c596fcaeed96517faf63a07c
Nilesoj20 <jvrn2014@gmail.com>
2023-07-01 16:34:38 -0700

    fix: test to the init method

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,15 +1,33 @@
+"""Testing of all base files, classes and methods"""
 import unittest
+import json
+from models.rectangle import Rectangle
 from models.base import Base
 
-class To_json_string(unittest.TestCase):
-    """comentario"""
 
-    def test_to_json_string_empty_list(self):
+class TestBase(unittest.TestCase):
+    """Test method to_json_string"""
+
+    def test_to_init(self):
+        """Method inicial """
+        b1 = Base()
+        b2 = Base(20)
+        self.assertEqual(1, b1.id)
+        self.assertEqual(20, b2.id)
+
+    def test_to_json_string_(self):
+        """Method to_json_string """
+        r1 = Rectangle(10, 7, 2, 8)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+ 
         self.assertEqual("[]", Base.to_json_string([]))
-
-    def test_to_json_string_none(self):
         self.assertEqual("[]", Base.to_json_string(None))
+        self.assertEqual(str, type(json_dictionary))
+        
+        js = json.dumps([{'width': 5, 'height': 8}])
+        self.assertEqual(js, Base.to_json_string([{'width': 5, 'height': 8}]))
 
-
+    
 if __name__ == '__main__':
     unittest.main()
