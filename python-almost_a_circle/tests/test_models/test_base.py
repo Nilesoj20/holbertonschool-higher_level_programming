8bbdb364be55c6d7185e28eb8f6d7c7093431c05
Nilesoj20 <jvrn2014@gmail.com>
2023-07-01 20:00:39 -0700

    fix: test to the basic methods

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -3,6 +3,8 @@ import unittest
 import json
 from models.rectangle import Rectangle
 from models.base import Base
+from models.square import Square
+import pycodestyle
 
 
 class TestBase(unittest.TestCase):
@@ -12,7 +14,7 @@ class TestBase(unittest.TestCase):
         """Method inicial """
         b1 = Base()
         b2 = Base(20)
-        self.assertEqual(3, b1.id)
+        self.assertEqual(13, b1.id)
         self.assertEqual(20, b2.id)
 
     def test_to_json_string_(self):
@@ -42,13 +44,61 @@ class TestBase(unittest.TestCase):
         Rectangle.save_to_file(None)
         with open("Rectangle.json", "r") as file:
             self.assertEqual(file.readline(), '[]')
-        Rectangle.save_to_file([rec1, rec2])
-        file_1 = rec1.to_dictionary()
-        file_2 = rec2.to_dictionary()
-        file_total = [file_1, file_2]
-        str_json = Rectangle.to_json_string(file_total)
+
+        Rectangle.save_to_file([])
         with open("Rectangle.json", "r") as file:
-            self.assertEqual(file.readline(), str_json)
+            self.assertEqual(file.readline(), '[]')
+
+    def test_create(self):
+        """Method to create"""
+        r1 = Rectangle(3, 5, 1)
+        r1_d = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_d)
+        self.assertAlmostEqual(r1.id, 1)
+        self.assertAlmostEqual(r2.id, 1)
+        self.assertAlmostEqual(r2, r2)
+        self.assertFalse(r1 == r2)
+        self.assertFalse(r1 is r2)
+
+    def test_load_from_file(self):
+        """Method load_from_file """
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        list_input = [r1, r2]
+        Rectangle.save_to_file(list_input)
+        list_output = Rectangle.load_from_file()
+        self.assertAlmostEqual(list_output[0].id, 3)
+        self.assertAlmostEqual(list_output[0].x, 2)
+        self.assertAlmostEqual(list_output[0].width, 10)
+        self.assertAlmostEqual(list_output[0].height, 7)
+        self.assertAlmostEqual(list_output[0].y, 8)
+        self.assertAlmostEqual(list_output[1].id, 4)
+        self.assertAlmostEqual(list_output[1].x, 0)
+        self.assertAlmostEqual(list_output[1].width, 2)
+        self.assertAlmostEqual(list_output[1].height, 4)
+        self.assertAlmostEqual(list_output[1].y, 0)
+        s1 = Square(5)
+        s2 = Square(7, 9, 1)
+        sinput = [s1, s2]
+        Square.save_to_file(sinput)
+        soutput = Square.load_from_file()
+        self.assertAlmostEqual(soutput[0].id, 7)
+        self.assertAlmostEqual(soutput[0].width, 5)
+        self.assertAlmostEqual(soutput[0].height, 5)
+        self.assertAlmostEqual(soutput[0].x, 0)
+        self.assertAlmostEqual(soutput[0].y, 0)
+        self.assertAlmostEqual(soutput[1].id, 8)
+        self.assertAlmostEqual(soutput[1].width, 7)
+        self.assertAlmostEqual(soutput[1].height, 7)
+        self.assertAlmostEqual(soutput[1].x, 9)
+        self.assertAlmostEqual(soutput[1].y, 1)
+
+    def test_pycodestyle_conformance(self):
+        """Test that we conform to PEP8."""
+        style = pycodestyle.StyleGuide(quiet=True)
+        result = style.check_files(['models/base.py',
+                                    'tests/test_models/test_base.py'])
+        self.assertEqual(result.total_errors, 0, "Found errors")
 
 
 if __name__ == '__main__':
